87efd995f8c5207568457ff62e6265fdbb38717e
davidevigolo <davide.vigolo.04@gmail.com>
2025-12-24 12:31:35 +0100

    correzione CI

--- scripts/check_docs.py
+++ scripts/check_docs.py
@@ -20,7 +20,7 @@ def calculate_gulpease(text):
     #clean_text = re.sub(r'\?{2,}', '', clean_text) # Rimuove sequenze di punti interrogativi
     # Mantiene solo lettere (inclusi accenti), numeri, spazi e punteggiatura base
     clean_text = re.sub(r'[^\w\s\.\,\!\?\'\’]', '', clean_text, flags=re.UNICODE) # Rimuove caratteri speciali
-    print(clean_text)
+    #print(clean_text)
     
     sentences = len(re.findall(r'[.!?]+', clean_text)) or 1
     words = len(re.findall(r'\w+', clean_text)) or 1
@@ -40,6 +40,7 @@ def check_latex(filepath):
         stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True,
         cwd=filepath_dir
     )
+    print(result.stdout)
     return result.returncode == 0
 
 def check_spelling(text):
@@ -98,7 +99,6 @@ def main():
         errors = check_spelling(content)
         if errors:
             print(f"❌ ERRORE: Trovati {len(errors)} errori di spelling.")
-            #print(f"Parole errate: {', '.join(list(errors))}")
             parole_errate_totali.update(errors)
             overall_success = False
         else:
@@ -106,7 +106,8 @@ def main():
 
     if not overall_success:
         print("\nUno o più controlli falliti. Il merge è bloccato.")
-        print(f"Parole errate totali trovate: \n{'\n'.join(list(parole_errate_totali))}\n")
+        parole_errate = '\n'.join(list(parole_errate_totali))
+        print(f"Parole errate totali trovate: \n{parole_errate}\n")
         sys.exit(1)
     else:
         print("\nTutti i controlli passati con successo!")
