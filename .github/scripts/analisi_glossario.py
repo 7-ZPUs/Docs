bbc4df3608d931e388d89a270bec620616304c03
Obliteratore <aaron.gingillino@gmail.com>
2026-04-23 17:01:39 +0200

    correzione

--- .github/scripts/analisi_glossario.py
+++ .github/scripts/analisi_glossario.py
@@ -6,7 +6,7 @@ from datetime import datetime
 # --- CONFIGURAZIONE ---
 # Percorso relativo per arrivare alla cartella dei documenti dallo script:
 PATH_DOCUMENTI = os.path.join("..", "..", "3_PB", "DocumentiInterni") 
-NOME_FILE_GLOSSARIO = "glossario.tex"
+NOME_FILE_GLOSSARIO = "Glossario.tex"
 OUTPUT_LOG = "log_glossario.txt" 
 COMANDO_GLOSSARIO_DOCS = r'\\glossario\{' 
 REGEX_DEFINIZIONE_GLOSSARIO = r'\\item\s*\\textbf\{(.+?)\}'
